087685eedc089df3c36083242e7507b1b74bf7ee
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2018-03-06 16:33:24 +0530

    magic string

---
+++ src/MagicString.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Question
+ Given a string. Return missing characters in the string to make it a magic string
+ Magic String: A String is said to magic string if it contains all the english alphabets-case insensitive
+ Input: "I am from Bhimavaram"
+ Ouptut: "cdegjklnpqstuwxyz"
+
+ Input:"abceghijkmnopqruv"
+ Output:"xyz"
+ */
+
+public class MagicString {
+
+    public static String missingCharsToMakeMagicString(String input){
+        ArrayList<Character> alphabets = new ArrayList<Character>();
+        for(Integer i = 97; i < 123; i++){
+            alphabets.add(Character.toChars(i)[0]);
+        }
+        for(Integer i = 0; i < input.length(); i++){
+            alphabets.remove(new Character(input.charAt(i)));
+        }
+        return alphabets.toString().replaceAll("[,\\s\\[\\]]", "");
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(MagicString.missingCharsToMakeMagicString("abceghijkmnopqruv"));
+        System.out.println(MagicString.missingCharsToMakeMagicString("I am from Bhimavaram"));
+    }
+
+}
